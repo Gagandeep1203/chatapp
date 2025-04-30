19216490d99a207545fe3d52faf81317ecfacbdf
Gagan <100506429+Gagandeep1203@users.noreply.github.com>
2025-04-30 12:30:04 +0530

    Delete chatapp directory

--- chatapp/api/models/users.js
+++
@@ -1,8 +0,0 @@
-const mongoose= require("mongoose");
-const schema=new mongoose.Schema({
-    username: {type: String , unique: true},
-    password: String 
-},{timestamps: true})  
-const userModel = mongoose.model('user', schema);
-
-module.exports = userModel; // Use CommonJS export 
